cfa810037301a10bc2d3fe170df4fe66e663371d
Farnoosh J <farnoosh.j80@gmail.com>
2023-06-21 15:43:31 +0330

    add proof of work

--- block.py
+++ block.py
@@ -3,15 +3,39 @@ import json
 
 
 class Block:
-    def __init__(self, index, timestamp, previous_block_hash, message_list):
-        self.index = index
+    def __init__(
+        self, timestamp, previous_block_hash, message="", difficulty=2, nonce=0
+    ):
         self.timestamp = timestamp
         self.previous_block_hash = previous_block_hash
-        self.message_list = message_list
+        self.message = message
+        self.difficulty = difficulty
+        self.nonce = nonce
+        self.mine()
 
     def compute_hash(self):
         block_data = json.dumps(self.__dict__, sort_keys=True)
+        block_data = (
+            str(self.timestamp)
+            + str(self.previous_block_hash)
+            + self.message
+            + str(self.difficulty)
+            + str(self.nonce)
+        )
         return sha256(block_data.encode()).hexdigest()
+
+    def mine(self):
+        self.hash = self.compute_hash()
+        # proof of work
+        while not self.hash.startswith("0" * self.difficulty):
+            self.nonce += 1
+            self.hash = self.compute_hash()
+
+    def validate_proof(self):
+        return (
+            self.hash.startswith("0" * self.difficulty)
+            and self.hash == self.compute_hash()
+        )
 
     def show_block(self):
         print(self.__dict__)
--- blockchain.py
+++ blockchain.py
@@ -4,12 +4,14 @@ from block import Block
 
 class Blockchain:
     def __init__(self, difficulty=2):
-        self.unconfirmed_messages = []
         self.chain = []
+        self.difficulty = difficulty
         self.create_initial_block()
 
     def create_initial_block(self):
-        initial_block = Block(0, time.time(), "0", [])
+        initial_block = Block(
+            timestamp=time.time(), previous_block_hash="0", difficulty=self.difficulty
+        )
         self.chain.append(initial_block)
 
     def last_block(self):
@@ -17,35 +19,29 @@ class Blockchain:
 
     def add_block(self, block: Block):
         last_block: Block = self.last_block()
-        previous_hash = last_block.compute_hash()
-        if previous_hash != block.previous_block_hash:
+        if block.previous_block_hash != last_block.hash:
+            return False
+        if not block.validate_proof():
             return False
         self.chain.append(block)
         return True
 
-    def add_new_message(self, message):
-        self.unconfirmed_messages.append(message)
-
-    def create_and_add_block(self):
-        if not self.unconfirmed_messages:
-            return False
-
+    def create_and_add_block(self, message):
         last_block: Block = self.last_block()
-
         new_block = Block(
-            index=last_block.index + 1,
             timestamp=time.time(),
-            previous_block_hash=last_block.compute_hash(),
-            message_list=self.unconfirmed_messages,
+            previous_block_hash=last_block.hash,
+            message=message,
+            difficulty=self.difficulty,
         )
-
+        if not new_block.validate_proof():
+            return False
         self.add_block(new_block)
-        self.unconfirmed_messages = []
-        return new_block.index
+        return True
 
     def validate_blockchain(self):
         for i in range(len(self.chain) - 1):
-            if self.chain[i].compute_hash() != self.chain[i + 1].previous_block_hash:
+            if self.chain[i].hash != self.chain[i + 1].previous_block_hash:
                 return False
         return True
 
